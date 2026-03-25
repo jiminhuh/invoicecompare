b5f5642ff82ce4e5f8a4241f703af14d32277a58
Jimin Huh <33494757+jiminhuh@users.noreply.github.com>
2026-03-25 13:29:58 -0500

    Remove sitemap plugin to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://invoicecompare.vercel.app',
-  integrations: [sitemap()],
   build: {
     format: 'directory',
   },
