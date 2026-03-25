f7f84aca726c5675d5e07788d2bb2897f583f3a1
Jimin Huh <33494757+jiminhuh@users.noreply.github.com>
2026-03-25 13:55:16 -0500

    Add astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  output: 'static',
-  site: 'https://invoicecompare.vercel.app',
-  build: {
-    format: 'directory',
-  },
+  output: "static",
+  site: "https://invoicecompare.vercel.app",
+  build: { format: "directory" },
 });
